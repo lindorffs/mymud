dedf428cc5ebf6570be7d543f63ab6070bdea70e
Sean Lindorff <lindorffs1@gmail.com>
2025-08-30 22:12:30 -0700

    Reverted changes.

--- src/server/scripts/main_logic.lua
+++ src/server/scripts/main_logic.lua
@@ -12,11 +12,16 @@ last_client_update = 0
 function UpdateLobbyTick()
 	local current_ms = os.clock() * 1000
 	local elapsed_tick = current_ms - last_tick
+	local elapsed_client_update = current_ms - last_client_update
 	if elapsed_tick > 1000 then
 		PlayerUpdateTick(current_ms)
-		SendPlayerUpdate()
 		
 		print("SERVER UPDATE TICK")
 		last_tick = os.clock() * 1000
+	end
+	if elapsed_client_update > 2000 then
+		print("CLIENT UPDATE TICK")
+		SendPlayerUpdate()
+		last_client_update = os.clock() * 1000
 	end
 end
